b6fb2bf129c550d8e86bfb1c9af80e79688444a6
Randy Goldsmith <rgoldshz@gmail.com>
2023-06-21 02:30:44 -0500

    finished phase2

--- practice-for-week-04-tdd-style-long-practice-main/problems/number-fun.js
+++ practice-for-week-04-tdd-style-long-practice-main/problems/number-fun.js
@@ -1,9 +1,19 @@
 function returnsThree() {
   // Your code here
+  return 3;
 }
 
-function reciprocal(n) {
-  // Your code here
+function reciprocal(num) {
+  //checking to see if input is a number
+  if(typeof num !== 'number') {
+    throw new TypeError('input must be of type number');
+  }
+
+  if(num < 1 || num > 1000000) {
+    throw new RangeError('input must be greater than 1 and less than 1,000,000');
+  }
+
+  return 1 / num;
 }
 
 module.exports = {
--- practice-for-week-04-tdd-style-long-practice-main/test/number-fun-spec.js
+++ practice-for-week-04-tdd-style-long-practice-main/test/number-fun-spec.js
@@ -1,1 +1,28 @@
 // Your code here
+const chai = require('chai');
+const expect = chai.expect;
+const { returnsThree, reciprocal } = require('../problems/number-fun');
+
+describe('returnsThree()', () => {
+    it('should return three', () => {
+        expect(returnsThree()).to.equal(3);
+    })
+})
+
+describe('reciprocal(num)', () => {
+    it('should return the reciprocal of the input number', () => {
+        expect(reciprocal(5/2)).to.equal(2/5);
+        expect(reciprocal(25)).to.equal(1/25);
+        expect(reciprocal(5)).to.equal(1/5);
+    })
+
+    it('should throw an error if input is not within range', () => {
+        expect(() => reciprocal(-5)).to.throw(RangeError);
+        expect(() => reciprocal(5555555)).to.throw(RangeError);
+    })
+
+    it('should throw an error if input is not a number', () => {
+        expect(() => reciprocal('mom')).to.throw(TypeError);
+        expect(() => reciprocal([1,2,3])).to.throw(TypeError);
+    })
+})
