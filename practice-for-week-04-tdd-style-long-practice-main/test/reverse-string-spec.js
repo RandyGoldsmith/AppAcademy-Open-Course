fc5b7ace99f8197da9832b008fa221c58bde406b
Randy Goldsmith <rgoldshz@gmail.com>
2023-06-21 01:43:36 -0500

    finished phase1

--- practice-for-week-04-tdd-style-long-practice-main/test/reverse-string-spec.js
+++ practice-for-week-04-tdd-style-long-practice-main/test/reverse-string-spec.js
@@ -10,4 +10,10 @@ describe('reverseString(string)', () => {
     it('should return the reversed string', () => {
         expect(reverseString('fun')).to.equal('nuf');
     })
+
+    it('should throw TypeError if not passed in a string', () => {
+        expect(() => reverseString(55).to.throw(TypeError));
+        expect(() => reverseString([1,2,3]).to.throw(TypeError));
+        expect(() => reverseString({name: 'rory', age: 5}).to.throw(TypeError));
+    })
 })
