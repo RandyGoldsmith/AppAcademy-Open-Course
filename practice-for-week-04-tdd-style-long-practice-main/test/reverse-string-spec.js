c231117ee6c746d8b8485c78b4092e96feb90605
Randy Goldsmith <randyg@Randys-MacBook-Pro.local>
2023-06-20 19:14:34 -0500

    finished reverse-string

--- practice-for-week-04-tdd-style-long-practice-main/test/reverse-string-spec.js
+++ practice-for-week-04-tdd-style-long-practice-main/test/reverse-string-spec.js
@@ -1,1 +1,13 @@
-// Your code here
+// I require chai so I can use it inside the file
+const chai = require('chai');
+// extracting the asssertion style we want to use which is "expect"
+const expect = chai.expect;
+// import reverseString function 
+const reverseString = require('../problems/reverse-string');
+
+
+describe('reverseString(string)', () => {
+    it('should return the reversed string', () => {
+        expect(reverseString('fun')).to.equal('nuf');
+    })
+})
